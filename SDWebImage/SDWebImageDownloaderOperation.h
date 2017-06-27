818570f8d24b1e0675e0b96e6f523cbff3c36ad4
zeekit <evyasaf@zeekit.co>
2017-06-27 16:44:51 +0300

    ADD ;

--- SDWebImage/SDWebImageDownloaderOperation.h
+++ SDWebImage/SDWebImageDownloaderOperation.h
@@ -119,6 +119,6 @@ extern NSString * _Nonnull const SDWebImageDownloadFinishNotification;
  */
 - (BOOL)cancel:(nullable id)token;
 
-- (NSError*)getResponseErrorWith: (NSURLResponse*)response
+- (NSError*)getResponseErrorWith: (NSURLResponse*)response;
  
 @end
